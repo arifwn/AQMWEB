b195ef7ffa0951d412c069adc67a0dcb49489d3f
Arif Widi Nugroho <arif@sainsmograf.com>
2011-12-20 13:50:35 +0700

    modified wrf models. mograte/wipe db before use

--- settings.py
+++ settings.py
@@ -11,14 +11,17 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'aqmweb',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+SOUTH_TESTS_MIGRATE = False
+SKIP_SOUTH_TESTS = True
 
 LOGIN_REDIRECT_URL = '/'
 
