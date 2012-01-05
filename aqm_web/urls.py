b952adae5edaa545c2abfe93b942f328f7f80af0
Arif Widi Nugroho <arif@sainsmograf.com>
2012-01-06 05:38:44 +0700

    cleanup urlconf

--- aqm_web/urls.py
+++ aqm_web/urls.py
@@ -5,32 +5,32 @@ Created on Sep 18, 2011
 '''
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-    url(r'^$', 'aqm_web.views.generic.index', name='index'),
-    url(r'^status/$', 'aqm_web.views.generic.status', name='status'),
-    url(r'^wrf/new/$', 'aqm_web.views.wrf.new_task', name='wrf_new_task'),
-    url(r'^wrf/new-group/$', 'aqm_web.views.wrf.new_task_group', name='wrf_new_task_group'),
-    url(r'^wrf/new/popup-chem-data$', 'aqm_web.views.wrf.popup_new_task_chem_data', name='wrf_popup_new_task_chem_data'),
+urlpatterns = patterns('aqm_web.views.generic',
+    url(r'^$', 'index', name='index'),
+    url(r'^status/$', 'status', name='status'),
     
-    url(r'^wrf/task-list/$', 'aqm_web.views.wrf.task_list', name='wrf_task_list'),
-    url(r'^wrf/task-group-list/$', 'aqm_web.views.wrf.task_group_list', name='wrf_task_group_list'),
-    url(r'^wrf/task-group-details/$', 'aqm_web.views.wrf.task_group_details', name='wrf_task_group_details'),
-    url(r'^wrf/chem-data-list/$', 'aqm_web.views.wrf.chem_data_list', name='wrf_chem_data_list'),
-    url(r'^wrf/meteo-data-list/$', 'aqm_web.views.wrf.meteo_data_list', name='wrf_meteo_data_list'),
-    url(r'^wrf/model-setting-list/$', 'aqm_web.views.wrf.model_setting_list', name='wrf_model_setting_list'),
-    
-    (r'^plot/mercator$', 'aqm_web.views.plot.mercator'),
-    (r'^plot/lambert-conformal$', 'aqm_web.views.plot.lambert_conformal'),
-    
-    (r'^testview/$', 'aqm_web.views.generic.testview'),
-    (r'^testview-popup/$', 'aqm_web.views.generic.testview_popup'),
-    (r'^plot/test$', 'aqm_web.views.plot.test_plot'),
-    
-    # Remove this in production
-#    (r'^static/aqm_web/(.*)$', 'aqm_web.views.debug_static'),
-    
-    url(r'^web_api/get_namelist.([-\w]+)$', 'aqm_web.views.web_api.get_namelist', name='api_get_namelist'),
+    (r'^testview/$', 'testview'),
+    (r'^testview-popup/$', 'testview_popup'),
     
     #for testing purpose
-    (r'^plotview/$', 'aqm_web.views.generic.plotview'),
+    (r'^plotview/$', 'plotview'),
+)
+
+urlpatterns += patterns('aqm_web.views.wrf',
+    url(r'^wrf/new/$', 'new_task', name='wrf_new_task'),
+    url(r'^wrf/new-group/$', 'new_task_group', name='wrf_new_task_group'),
+    url(r'^wrf/new/popup-chem-data$', 'popup_new_task_chem_data', name='wrf_popup_new_task_chem_data'),
+    
+    url(r'^wrf/task-list/$', 'task_list', name='wrf_task_list'),
+    url(r'^wrf/task-group-list/$', 'task_group_list', name='wrf_task_group_list'),
+    url(r'^wrf/task-group-details/$', 'task_group_details', name='wrf_task_group_details'),
+    url(r'^wrf/chem-data-list/$', 'chem_data_list', name='wrf_chem_data_list'),
+    url(r'^wrf/meteo-data-list/$', 'meteo_data_list', name='wrf_meteo_data_list'),
+    url(r'^wrf/model-setting-list/$', 'model_setting_list', name='wrf_model_setting_list'),
+)
+
+urlpatterns += patterns('aqm_web.views.plot',
+    (r'^plot/mercator$', 'mercator'),
+    (r'^plot/lambert-conformal$', 'lambert_conformal'),
+    (r'^plot/test$', 'test_plot'),
 )
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,15 +6,17 @@ Created on Jan 5, 2012
 
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-    url(r'^profile/([-\w]+)$', 'user_profile.views.view_profile', name='view-profile'),
-    url(r'^edit/avatar$', 'user_profile.views.edit_avatar', name='edit-avatar'),
-    url(r'^edit/profile$', 'user_profile.views.edit_profile', name='edit-profile'),
-    
-    # handle user login
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
-    url(r'^change_password/$', 'django.contrib.auth.views.password_change'),
-    url(r'^change_password_done/$', 'django.contrib.auth.views.password_change_done'),
+urlpatterns = patterns('user_profile.views',
+    url(r'^profile/([-\w]+)$', 'view_profile', name='view-profile'),
+    url(r'^edit/avatar$', 'edit_avatar', name='edit-avatar'),
+    url(r'^edit/profile$', 'edit_profile', name='edit-profile'),
+)
+
+# handle user login
+urlpatterns += patterns('django.contrib.auth.views',   
+    url(r'^login/$', 'login'),
+    url(r'^logout/$', 'logout_then_login'),
+    url(r'^change_password/$', 'password_change'),
+    url(r'^change_password_done/$', 'password_change_done'),
     
 )
